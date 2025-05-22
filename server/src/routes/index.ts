6d3bfe6394675b5e791a93df71149680cace53dc
Brett Plemons <brett.plemons@gmail.com>
2025-05-22 09:12:10 -0500

    feat(routes): wire up travel routes to new controller
    - Update /travel and /travel/:id routes to use new controller.
    - File: server/src/routes/index.ts

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
-import { pagesController } from '../controllers/pagesController.js';
+import { pagesController } from '../controllers/pages.controller.js';
+import { getTripById, getTrips } from '../controllers/travel.controller.js';
 
 const router: express.Router = express.Router();
 
@@ -10,6 +11,9 @@ router.get('/contact', pagesController.getContactPage);
 router.get('/meals', pagesController.getMealsPage);
 router.get('/news', pagesController.getNewsPage);
 router.get('/rooms', pagesController.getRoomsPage);
-router.get('/travel', pagesController.getTravelPage);
+
+// Travel routes
+router.get('/travel', getTrips);
+router.get('/travel/:id', getTripById);
 
 export default router;
